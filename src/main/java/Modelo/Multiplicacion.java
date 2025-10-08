4a4cfef85127c8a8c2272a194b519c021ecd30b8
miguel82014a-ai <miguel82014a@gmail.com>
2025-10-08 10:47:54 -0500

    Añadi el codigo de la funcionalidad de Multiplicacion y rectifique con tester

--- src/main/java/Modelo/Multiplicacion.java
+++ src/main/java/Modelo/Multiplicacion.java
@@ -11,5 +11,34 @@ package Modelo;
 public class Multiplicacion {
     private double primerNumero;
     private double segundoNumero;
+    private double resultado;
+
+    public double getPrimerNumero() {
+        return primerNumero;
+    }
+
+    public void setPrimerNumero(double primerNumero) {
+        this.primerNumero = primerNumero;
+    }
+
+    public double getSegundoNumero() {
+        return segundoNumero;
+    }
+
+    public void setSegundoNumero(double segundoNumero) {
+        this.segundoNumero = segundoNumero;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
     
+    public double multiplicar (double primerNumero, double segundoNumero){
+        resultado=primerNumero*segundoNumero;
+        return resultado;
+    }
 }
