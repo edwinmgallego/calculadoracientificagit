4c18e799d81fe068192c6ed44cb573fe75772e19
Rango345 <juancamilosee@gmail.com>
2025-10-08 09:44:09 -0500

    Hice la clase de exponencial

---
+++ src/main/java/Modelo/Exponencial.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+
+/**
+ *
+ * @author oeara
+ */
+public class Exponencial {
+    
+    private double primerNumero;
+    private double resultado;
+    private double segundoNumero;
+    
+    public void Expocinencial() {
+        resultado = Math.pow (primerNumero,segundoNumero);
+    }
+    
+}
