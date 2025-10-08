ec8c7d93ea57cce82254cbb6aea48a73a61bec10
AngelloCortesPalomares <cortes.palomares.angello00@gmail.com>
2025-10-08 10:47:14 -0500

    Cambie el color de la letra de unos botones en el GUI y realice la funcion para operar la raiz cuadrada y lo rectifique con un tester

--- src/main/java/Modelo/RaizCuadrada.java
+++ src/main/java/Modelo/RaizCuadrada.java
@@ -10,7 +10,28 @@ package Modelo;
  */
 public class RaizCuadrada {
     private double primerNumero;
-    private String resultado;
+    private double resultado;
+
+    public double getPrimerNumero() {
+        return primerNumero;
+    }
+
+    public void setPrimerNumero(double primerNumero) {
+        this.primerNumero = primerNumero;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
     
-    
+    public double raiz(double primerNumero){
+        
+        resultado = Math.sqrt(primerNumero);
+        
+        return resultado;
+    }
 }
