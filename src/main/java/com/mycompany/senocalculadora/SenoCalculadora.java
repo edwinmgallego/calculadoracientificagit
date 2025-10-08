f50eb99334592f973889f89127b0b23263019cb9
Juandap22 <juandavid.pantoja22@gmail.com>
2025-10-08 09:32:46 -0500

    este es el seno (sin) de la calculadora

---
+++ src/main/java/com/mycompany/senocalculadora/SenoCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.senocalculadora;
+
+/**
+ *
+ * @author Juan David
+ */
+public class SenoCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
