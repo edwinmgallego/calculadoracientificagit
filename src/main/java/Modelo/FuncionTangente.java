86d0418e1ba0db3c18a5e2fea97d261403114bda
Miguel <miguelbv2302@gmail.com>
2025-10-13 15:16:02 -0500

    Se completó la función Tangente y se realizó su respectiva prueba de ejecución

--- src/main/java/Modelo/FuncionTangente.java
+++ src/main/java/Modelo/FuncionTangente.java
@@ -9,5 +9,24 @@ package Modelo;
  * @author Miguel
  */
 public class FuncionTangente {
+
     private double primerNumero;
+
+    public FuncionTangente(double primerNumero) {
+        this.primerNumero = primerNumero;
+    }
+
+    public double getPrimerNumero() {
+        return primerNumero;
+    }
+
+    public double calcularTangente() {
+        //Validación
+        if ((primerNumero - 90) % 180 == 0) {
+            System.out.println("Error: La tangente no está definida para " + primerNumero + "°");
+            return Double.NaN; // Retorna error
+        }
+        double radianes = Math.toRadians(primerNumero);
+        return Math.tan(radianes);
+    }
 }
