b41d499e8a48183192ae320f70a794ebadaad29f
Rango345 <juancamilosee@gmail.com>
2025-10-17 10:03:01 -0500

    Clase exponencial

--- src/main/java/Modelo/Exponencial.java
+++ src/main/java/Modelo/Exponencial.java
@@ -14,6 +14,31 @@ public class Exponencial {
     private double primerNumero;
     private double resultado;
     private double segundoNumero;
+
+    public double getPrimerNumero() {
+        return primerNumero;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public double getSegundoNumero() {
+        return segundoNumero;
+    }
+
+    public void setPrimerNumero(double primerNumero) {
+        this.primerNumero = primerNumero;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    public void setSegundoNumero(double segundoNumero) {
+        this.segundoNumero = segundoNumero;
+    }
+    
     
     public double Expocinencial(double primerNumero, double segundoNumero) {
         resultado = Math.pow (primerNumero,segundoNumero);
