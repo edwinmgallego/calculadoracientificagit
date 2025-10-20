2fc1e1f86728bbbcb555a7636a468d5263291bc8
Juan2907cc <juanjosecc29@gmail.com>
2025-10-20 10:45:26 -0500

    este es mi primet commit

---
+++ OneDrive/Documentos/NetBeansProjects/Figuras geometricas/CalculadoraGit/src/main/java/com/mycompany/calculadoragit/CalculadoraGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoragit;
+
+/**
+ *
+ * @author juanj
+ */
+public class CalculadoraGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
