d6e1ce6d8334690a9b7a42668cfe512e14e13bd2
mxom671 <maria_val.ospina@uao.edu.co>
2025-10-20 11:07:40 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/calculadoragrupalpromedio/CalculadoraGrupalPromedio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoragrupalpromedio;
+
+/**
+ *
+ * @author luisb
+ */
+public class CalculadoraGrupalPromedio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
