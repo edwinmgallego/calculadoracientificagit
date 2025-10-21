c6d461300b309292507113c7cc6357b691fc9f74
veronz07 <veronica.nunez@uao.edu.co>
2025-10-20 22:22:19 -0500

    Creación de la clase Euler para ser usada como constante matemática para las operaciones.

---
+++ src/main/java/Modelo/Euler.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Vero
+ */
+public class Euler {
+    
+    public static final double E = Math.E;
+
+    public static double getE() {
+        return E;
+    }
+}
