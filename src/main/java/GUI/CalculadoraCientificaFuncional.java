623bea1609295d7efdfcb3639b7a0fd0cf595c4a
CamiArtG12 <kmigamer16@gmail.com>
2025-10-22 11:32:32 -0500

    hice un nuevo repositorio con una clase main llamada ColegioApp cree un paquete modelo en el que estan alli las clases Estudiante, Profesor, Persona, ProfeCatedra y ProfeATiempoCompleto. haciendo codigo en las clases Persona, Profesor, y Estudiante termine los codigos de las clases anteriormente dichas, y ahora avance la clase ProfeTiempoCompleto las subclases de profesor ya casi estan termniada Editando la clase main ColegioApp, se le agregó un menu el cual organizara la informacion que se solicitó haciendo los test unitarios y de integracion mandando a la rama fix11

--- src/main/java/GUI/CalculadoraCientificaFuncional.java
+++ src/main/java/GUI/CalculadoraCientificaFuncional.java
@@ -141,7 +141,7 @@ public class CalculadoraCientificaFuncional extends JFrame implements ActionList
                      primerNumero = Double.parseDouble(textoDisplay);
                     display.setText(String.valueOf(Math.cbrt(primerNumero)));
                     break;
-                case "x²": // Este no estaba en la lista pero es común, lo añado como ejemplo
+                case "x²": // Este no estaba en la lista pero es común, lo añado como ejemplo1
                     primerNumero = Double.parseDouble(textoDisplay);
                     display.setText(String.valueOf(primerNumero * primerNumero));
                     break;
